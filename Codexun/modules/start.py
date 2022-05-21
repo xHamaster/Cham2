d1ec27a2f668c7cd629b65d34de214378f28d48f
xHamaster <105044443+xHamaster@users.noreply.github.com>
2022-05-21 09:10:53 +0530

    Update start.py

--- Codexun/modules/start.py
+++ Codexun/modules/start.py
@@ -6,9 +6,8 @@ from pytgcalls import (__version__ as pytover)
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, ChatJoinRequest
 from Codexun.utils.filters import command
 
-from Codexun.config import BOT_USERNAME
-
-
+from Codexun.config import BOT_USERNAME 
+from Codexun.config import BOT_NAME
 
 @Client.on_message(command("start") & filters.private & ~filters.edited)
 async def start_(client: Client, message: Message):
@@ -16,7 +15,7 @@ async def start_(client: Client, message: Message):
         photo=f"https://telegra.ph/file/9d1ee651cb815e49fb6ad.jpg",
         caption=f"""**Welcome {message.from_user.mention()}** 👋
 
-This is the broken music bot, a bot for playing high quality and unbreakable music in your groups voice chat.
+This is the from [{BOT_NAME}](https://t.me/{BOT_USERNAME}), a bot for playing high quality and unbreakable music in your groups voice chat.
 
 Just add me to your group and make a admin with needed admin permission to perform a right actions !
 
